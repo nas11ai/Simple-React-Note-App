1149c3e44fc285ec771e78edc0aec3117646dbe4
nas11ai <74126181+nas11ai@users.noreply.github.com>
2022-04-11 21:26:49 +0800

    adding database connection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-import { useState } from 'react'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
+
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
 
-    setNotes([...notes, noteObject])
+    setNotes(notes.concat(noteObject))
     setNewNote('')
   }
 
   const handleNoteChange = (event) => {
+    console.log(event.target.value)
     setNewNote(event.target.value)
   }
 
@@ -41,7 +56,10 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
       </form>
     </div>
